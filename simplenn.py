4d5fc3610c0d8d8366db9c104ee9dbef7dc55061
tfchan <cseelemon@gmail.com>
2019-10-18 13:35:05 +0800

    Add activation function

--- simplenn.py
+++ simplenn.py
@@ -4,11 +4,25 @@
 import numpy as np
 
 
+class Activation:
+    """Activation function class."""
+
+    @staticmethod
+    def linear(x):
+        """Perform linear activation."""
+        return x
+
+    @staticmethod
+    def sigmoid(x):
+        """Perform sigmoid activation."""
+        return 1 / (1 + np.exp(-x))
+
+
 class Layer:
     """A NN layer."""
 
-    def __init__(self, units, input_shape, activation=None):
+    def __init__(self, units, input_shape, activation='linear'):
         """Initialize layer."""
         self._weight = np.random.rand(input_shape, units)
         self._bias = np.random.rand(units)
-        self.activation = activation
+        self.activation = getattr(Activation, activation)
