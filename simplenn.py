b40a26745d2fd22ba67daa4257199112906497b6
tfchan <cseelemon@gmail.com>
2019-10-18 18:49:04 +0800

    Make input_shape an optional parameter

--- simplenn.py
+++ simplenn.py
@@ -18,17 +18,28 @@ class Activation:
         return 1 / (1 + np.exp(-x))
 
 
-class Layer:
-    """A NN layer."""
+class Dense:
+    """A Dense layer."""
 
-    def __init__(self, units, input_shape, activation='linear'):
+    def __init__(self, units, input_shape=None, activation='linear'):
         """Initialize layer."""
-        self._weight = np.random.rand(units, input_shape)
+        self._units = units
+        self._input_shape = input_shape
+        self._weight = None
+        if self._input_shape is not None:
+            self.init_weight()
         self._bias = np.random.rand(units)
         self._activation = getattr(Activation, activation)
 
+    def init_weight(self):
+        """Initialize weight of this layer."""
+        self._weight = np.random.rand(self._units, self._input_shape)
+
     def forward(self, x):
         """Compute wx+b, and activate."""
+        if self._input_shape is None:
+            self._input_shape = x.shape[-1]
+            self.init_weight()
         z = np.dot(self._weight, x) + self._bias
         z = self._activation(z)
         return z
@@ -45,7 +56,7 @@ class SimpleNN:
         """Add a layer to the model."""
         self._layers.append(layer)
 
-    def _feed_forward(self, x):
+    def _forward(self, x):
         """Feed forward through all layers."""
         for layer in self._layers:
             x = layer.forward(x)
